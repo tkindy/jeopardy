902b2957f9a80448eaac68bee2735df4e6049760
Tyler Kindy <tylerkindy@gmail.com>
2022-12-17 14:26:37 -0500

    Keep clue in an atom

--- src/com/tylerkindy/jeopardy/main.clj
+++ src/com/tylerkindy/jeopardy/main.clj
@@ -5,20 +5,26 @@
             [hiccup.core :refer [html]]
             [com.tylerkindy.jeopardy.jservice :refer [random-clue]]))
 
-(defn random-clue-html []
-  (let [{:keys [question]} (random-clue)]
-    (html
-     [:div#question question])))
+(defonce clue (atom nil))
+
+(defn render-question [{:keys [question]}]
+  (html
+   [:div#question question]))
 
 (defn send-clue [ch]
-  (send! ch (random-clue-html)))
+  (send! ch (render-question @clue)))
+
+(defn new-clue [ch]
+  (let [new (random-clue)]
+    (reset! clue new)
+    (send-clue ch)))
 
 (defn app [req]
   (if-not (:websocket? req)
     (routes req)
     (as-channel req
-                {:on-open send-clue
-                 :on-receive (fn [ch _] (send-clue ch))})))
+                {:on-open new-clue
+                 :on-receive (fn [ch _] (new-clue ch))})))
 
 (defn start-server []
   (run-server app {:port 8080
