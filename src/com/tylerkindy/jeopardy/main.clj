7aa12fb723b98ffe890fc888f055ae77c3f7c572
Tyler Kindy <tylerkindy@gmail.com>
2022-12-17 14:20:29 -0500

    Send random clues

--- src/com/tylerkindy/jeopardy/main.clj
+++ src/com/tylerkindy/jeopardy/main.clj
@@ -2,14 +2,23 @@
   (:require [mount.core :refer [defstate] :as mount]
             [org.httpkit.server :refer [run-server as-channel send! server-stop!]]
             [com.tylerkindy.jeopardy.routes :refer [routes]]
-            [hiccup.core :refer [html]]))
+            [hiccup.core :refer [html]]
+            [com.tylerkindy.jeopardy.jservice :refer [random-clue]]))
+
+(defn random-clue-html []
+  (let [{:keys [question]} (random-clue)]
+    (html
+     [:div#question question])))
+
+(defn send-clue [ch]
+  (send! ch (random-clue-html)))
 
 (defn app [req]
   (if-not (:websocket? req)
     (routes req)
     (as-channel req
-                {:on-open (fn [ch]
-                            (send! ch (html [:div#question "Loaded over WS"])))})))
+                {:on-open send-clue
+                 :on-receive (fn [ch _] (send-clue ch))})))
 
 (defn start-server []
   (run-server app {:port 8080
