ac6d64dc6596a601fa528ac30bd427f9eba2a701
Tyler Kindy <tylerkindy@gmail.com>
2022-12-17 14:04:15 -0500

    Send example message

--- src/com/tylerkindy/jeopardy/main.clj
+++ src/com/tylerkindy/jeopardy/main.clj
@@ -1,13 +1,15 @@
 (ns com.tylerkindy.jeopardy.main
   (:require [mount.core :refer [defstate] :as mount]
             [org.httpkit.server :refer [run-server as-channel send! server-stop!]]
-            [com.tylerkindy.jeopardy.routes :refer [routes]]))
+            [com.tylerkindy.jeopardy.routes :refer [routes]]
+            [hiccup.core :refer [html]]))
 
 (defn app [req]
   (if-not (:websocket? req)
     (routes req)
     (as-channel req
-                {:on-receive (fn [ch message] (send! ch message))})))
+                {:on-open (fn [ch]
+                            (send! ch (html [:div#question "Loaded over WS"])))})))
 
 (defn start-server []
   (run-server app {:port 8080
