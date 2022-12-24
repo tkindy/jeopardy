c281222bf9a984897d335480f5b34d1362b6bd0e
Tyler Kindy <tylerkindy@gmail.com>
2022-12-24 16:08:11 -0500

    Add sample game page route

--- src/com/tylerkindy/jeopardy/games.clj
+++ src/com/tylerkindy/jeopardy/games.clj
@@ -1,5 +1,5 @@
 (ns com.tylerkindy.jeopardy.games
-  (:require [compojure.core :refer [defroutes context POST]]))
+  (:require [compojure.core :refer [defroutes context POST GET]]))
 
 (defn create-game [{:keys [params]}]
   (println params)
@@ -7,6 +7,12 @@
    :headers {"Content-Type" "text/html"}
    :body "<p>Thanks!</p>"})
 
+(defn game-page-response [game-id]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (str "<p>Game: " game-id "</p>")})
+
 (defroutes game-routes
   (context "/games" []
-    (POST "/" request (create-game request))))
+    (POST "/" request (create-game request))
+    (GET "/:game-id" [game-id] (game-page-response game-id))))
