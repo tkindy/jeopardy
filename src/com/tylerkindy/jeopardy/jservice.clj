be0310a46c9b15994ca2525228baa2b825a14420
Tyler Kindy <tylerkindy@gmail.com>
2022-12-27 12:02:43 -0500

    Filter out audio clues and Daily Doubles

--- src/com/tylerkindy/jeopardy/jservice.clj
+++ src/com/tylerkindy/jeopardy/jservice.clj
@@ -2,12 +2,15 @@
   (:require [clj-http.client :as http]
             [clojure.string :as str]))
 
+(defn valid-clue? [{:keys [question value]}]
+  (let [question (str/lower-case question)]
+    (and value
+         (not (str/includes? question "seen here"))
+         (not (str/includes? question "heard here")))))
+
 (defn random-clue []
   (->> (http/get "https://jservice.io/api/random?count=10"
                  {:accept :json, :as :json})
        :body
-       (filter #(-> %
-                    str/lower-case
-                    (str/includes? "seen here")
-                    not))
+       (filter valid-clue?)
        first))
