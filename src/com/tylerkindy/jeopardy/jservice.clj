125875a3015a32fda7984207eeffcaf5481a97cf
Tyler Kindy <tylerkindy@gmail.com>
2022-12-27 22:49:22 -0500

    Filter out single-letter answers

--- src/com/tylerkindy/jeopardy/jservice.clj
+++ src/com/tylerkindy/jeopardy/jservice.clj
@@ -1,7 +1,7 @@
 (ns com.tylerkindy.jeopardy.jservice
   (:require [clj-http.client :as http]
             [clojure.string :as str]
-            [com.tylerkindy.jeopardy.answer :refer [normalize-answer]]))
+            [com.tylerkindy.jeopardy.answer :refer [char-pairs normalize-answer]]))
 
 (defn valid-clue? [{:keys [question value]}]
   (let [question (str/lower-case question)]
@@ -9,12 +9,16 @@
          (not (str/includes? question "seen here"))
          (not (str/includes? question "heard here")))))
 
+(defn valid-answer? [{:keys [answer]}]
+  (seq (char-pairs answer)))
+
 (defn random-clues [n]
   (->> (http/get (str "https://jservice.io/api/random?count=" n)
                  {:accept :json, :as :json})
        :body
        (filter valid-clue?)
-       (map (fn [clue] (update clue :answer normalize-answer)))))
+       (map (fn [clue] (update clue :answer normalize-answer)))
+       (filter valid-answer?)))
 
 (defn random-clue []
   (first (random-clues 10)))
