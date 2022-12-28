f477048fac48b57239e490ebf73760ed59202bdc
Tyler Kindy <tylerkindy@gmail.com>
2022-12-27 22:17:25 -0500

    Transliterate answers

--- src/com/tylerkindy/jeopardy/answer.clj
+++ src/com/tylerkindy/jeopardy/answer.clj
@@ -1,11 +1,23 @@
 (ns com.tylerkindy.jeopardy.answer
-  (:import [org.jsoup Jsoup]
+  (:require [clojure.string :as str])
+  (:import [java.text Normalizer Normalizer$Form]
+           [org.jsoup Jsoup]
            [org.jsoup.safety Safelist]))
 
 (defn strip-html [answer]
   (Jsoup/clean answer (Safelist/none)))
 
+; Ported from https://stackoverflow.com/a/16179878/5812203
+(defn transliterate [answer]
+  (-> answer
+      (Normalizer/normalize Normalizer$Form/NFKD)
+      ; https://www.unicode.org/versions/Unicode14.0.0/ch04.pdf
+      ; Section 4.5, page 173
+      (str/replace #"(\p{Lu}|\p{Ll}|\p{Lt}|\p{Lm}|\p{Lo})(?:\p{Mn}|\p{Mc}|\p{Me})+"
+                   (fn [[_ letter]] letter))))
+
 ; TODO: implement edit distance
 (defn normalize-answer [answer]
   (-> answer
-      strip-html))
+      strip-html
+      transliterate))
