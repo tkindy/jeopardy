7213829bde0c38de76be93dc8f1f48b0952e4152
Tyler Kindy <tylerkindy@gmail.com>
2023-01-06 22:46:05 -0500

    Escape HTML in name

--- src/com/tylerkindy/jeopardy/players.clj
+++ src/com/tylerkindy/jeopardy/players.clj
@@ -1,11 +1,12 @@
 (ns com.tylerkindy.jeopardy.players
   (:require [compojure.core :refer [defroutes POST]]
             [com.tylerkindy.jeopardy.db.players :refer [insert-player]]
-            [com.tylerkindy.jeopardy.db.core :refer [ds]]))
+            [com.tylerkindy.jeopardy.db.core :refer [ds]]
+            [hiccup.util :refer [escape-html]]))
 
 (defn create-player [{:keys [params session]}]
   (let [{:keys [game-id name]} params
-        result (insert-player ds {:game-id game-id, :name name})
+        result (insert-player ds {:game-id game-id, :name (escape-html name)})
         id (get-in result [0 :id])]
     {:status 303
      :session (assoc session :id id)
