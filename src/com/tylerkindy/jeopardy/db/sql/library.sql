15554f58c91dc88bad95fd92fa6dc886ed277b76
Tyler Kindy <tylerkindy@gmail.com>
2024-07-24 23:07:24 -0400

    Add library category queries

--- src/com/tylerkindy/jeopardy/db/sql/library.sql
+++ src/com/tylerkindy/jeopardy/db/sql/library.sql
@@ -11,3 +11,23 @@ join categories as ca on cl.category_id = ca.id
 join games as g on cl.game_id = g.id
 order by random()
 limit :limit
+
+-- :name get-random-category :? :1
+select * from categories
+order by random()
+limit 1;
+
+-- :name get-random-game-with-category :? :1
+select distinct(game_id)
+from clues
+where category_id = :category-id
+order by random()
+limit 1;
+
+-- :name get-next-category-clue :? :1
+select * from clues
+where game_id = :game-id
+  and category_id = :category-id
+  and value > :last-value
+order by value
+limit 1;
