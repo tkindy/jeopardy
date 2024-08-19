b7718c2bf834ba4e82033d84b180f9107bf86abe
Tyler Kindy <tylerkindy@gmail.com>
2024-08-18 22:32:24 -0400

    Add query to get current clue guesses

--- src/com/tylerkindy/jeopardy/db/sql/guesses.sql
+++ src/com/tylerkindy/jeopardy/db/sql/guesses.sql
@@ -1,3 +1,19 @@
 -- :name insert-guess :! :n
 INSERT INTO guesses (clue_id, player_id, guess, correct, guessed_at)
 VALUES (:clue-id, :player-id, :guess, :correct, :guessed-at);
+
+-- :name get-current-guesses :?
+SELECT
+  g.id,
+  g.guess,
+  p.name as player,
+  g.correct
+FROM guesses AS g
+JOIN players AS p ON g.player_id = p.id
+WHERE g.clue_id = (
+  SELECT id FROM endless_clues
+  WHERE game_id = :game-id
+  ORDER BY id DESC
+  LIMIT 1
+)
+ORDER BY g.id;
